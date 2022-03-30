02865714239cb425e9e1f1fd8b4a6af7174d7a8c
AnnisaFitry <2041720123@student.polinema.ac.id>
2022-03-30 17:26:52 +0700

    edited index

--- index.php
+++ index.php
@@ -88,6 +88,23 @@
                                     </li>
                                 </ul> -->
                             </li>
+
+                            <li class="menu single-menu">
+                                <a href="#" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+                                    <div class="">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-clipboard">
+                                            <path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path>
+                                            <rect x="8" y="2" width="8" height="4" rx="1" ry="1"></rect>
+                                        </svg>
+
+                                        <span>Kategori</span>
+                                    </div>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down">
+                                        <polyline points="6 9 12 15 18 9"></polyline>
+                                    </svg>
+                                </a>
+                            </li>
+
                             <li class="menu single-menu">
                                 <a href="#" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
                                     <div class="">
