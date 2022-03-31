ea59f3d92ff7359071fe1407132faef50803fbd0
Farah14-lab <zulfafara1@gmail.com>
2022-03-31 13:50:39 +0700

    login Pages New

--- sql/webinar_collection.sql
+++ sql/webinar_collection.sql
@@ -1,111 +1,187 @@
-/*==============================================================*/
-/* DBMS name:      MySQL 5.0                                    */
-/* Created on:     17/03/2022 09:50:21                          */
-/*==============================================================*/
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 30 Mar 2022 pada 09.21
+-- Versi server: 10.4.21-MariaDB
+-- Versi PHP: 8.0.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
-drop table if exists ACC_WEBINAR;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-drop table if exists KATEGORI;
+--
+-- Database: `webinar_collection`
+--
 
-drop table if exists USER;
+-- --------------------------------------------------------
 
-drop table if exists WEBINAR;
+--
+-- Struktur dari tabel `acc_webinar`
+--
 
-drop table if exists WEBINAR_KATEGORI;
+CREATE TABLE `acc_webinar` (
+  `USER_ID` int(255) NOT NULL,
+  `WEBINAR_ID` int(255) NOT NULL,
+  `PESAN` text DEFAULT NULL,
+  `STATUS_PROPOSAL` int(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-drop table if exists WEBINAR_REGIST;
+-- --------------------------------------------------------
 
-/*==============================================================*/
-/* Table: ACC_WEBINAR                                           */
-/*==============================================================*/
-create table ACC_WEBINAR
-(
-   USER_ID              int(255) not null,
-   WEBINAR_ID           int(255) not null,
-   PESAN                text,
-   STATUS_PROPOSAL      int(1),
-   primary key (USER_ID, WEBINAR_ID)
-);
+--
+-- Struktur dari tabel `kategori`
+--
 
-/*==============================================================*/
-/* Table: KATEGORI                                              */
-/*==============================================================*/
-create table KATEGORI
-(
-   KATEGORI_ID          int(3) not null,
-   NAMA_KATEGORI        varchar(50),
-   ICON_KATEGORI        varchar(200),
-   primary key (KATEGORI_ID)
-);
+CREATE TABLE `kategori` (
+  `KATEGORI_ID` int(3) NOT NULL,
+  `NAMA_KATEGORI` varchar(50) DEFAULT NULL,
+  `ICON_KATEGORI` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-/*==============================================================*/
-/* Table: USER                                                  */
-/*==============================================================*/
-create table USER
-(
-   USER_ID              int(255) not null,
-   NAMA                 varchar(200),
-   EMAIL                varchar(100),
-   PASSWORD             varchar(256),
-   ROLE                 int(1),
-   primary key (USER_ID)
-);
+-- --------------------------------------------------------
 
-/*==============================================================*/
-/* Table: WEBINAR                                               */
-/*==============================================================*/
-create table WEBINAR
-(
-   WEBINAR_ID           int(255) not null,
-   USER_ID              int(255),
-   JUDUL_WEBINAR        varchar(200),
-   DESKRIPSI_WEBINAR    text,
-   WAKTU_WEBINAR        datetime,
-   MAKS_KAPASITAS       int(3),
-   LINK_MEETING         text,
-   COVER_WEBINAR        varchar(200),
-   primary key (WEBINAR_ID)
-);
+--
+-- Struktur dari tabel `user`
+--
 
-/*==============================================================*/
-/* Table: WEBINAR_KATEGORI                                      */
-/*==============================================================*/
-create table WEBINAR_KATEGORI
-(
-   KATEGORI_ID          int(3) not null,
-   WEBINAR_ID           int(255) not null,
-   primary key (KATEGORI_ID, WEBINAR_ID)
-);
+CREATE TABLE `user` (
+  `USER_ID` int(255) NOT NULL,
+  `NAMA` varchar(200) DEFAULT NULL,
+  `EMAIL` varchar(100) DEFAULT NULL,
+  `PASSWORD` varchar(256) DEFAULT NULL,
+  `ROLE` int(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-/*==============================================================*/
-/* Table: WEBINAR_REGIST                                        */
-/*==============================================================*/
-create table WEBINAR_REGIST
-(
-   USER_ID              int(255) not null,
-   WEBINAR_ID           int(255) not null,
-   primary key (USER_ID, WEBINAR_ID)
-);
+--
+-- Dumping data untuk tabel `user`
+--
 
-alter table ACC_WEBINAR add constraint FK_ACC_WEBINAR foreign key (USER_ID)
-      references USER (USER_ID) on delete restrict on update restrict;
+INSERT INTO `user` (`USER_ID`, `NAMA`, `EMAIL`, `PASSWORD`, `ROLE`) VALUES
+(10, 'farah', 'zulfafara1@gmail.com', '1414', 1);
 
-alter table ACC_WEBINAR add constraint FK_ACC_WEBINAR2 foreign key (WEBINAR_ID)
-      references WEBINAR (WEBINAR_ID) on delete restrict on update restrict;
+-- --------------------------------------------------------
 
-alter table WEBINAR add constraint FK_CREATE_WEBINAR foreign key (USER_ID)
-      references USER (USER_ID) on delete restrict on update restrict;
+--
+-- Struktur dari tabel `webinar`
+--
 
-alter table WEBINAR_KATEGORI add constraint FK_WEBINAR_KATEGORI foreign key (KATEGORI_ID)
-      references KATEGORI (KATEGORI_ID) on delete restrict on update restrict;
+CREATE TABLE `webinar` (
+  `WEBINAR_ID` int(255) NOT NULL,
+  `USER_ID` int(255) DEFAULT NULL,
+  `JUDUL_WEBINAR` varchar(200) DEFAULT NULL,
+  `DESKRIPSI_WEBINAR` text DEFAULT NULL,
+  `WAKTU_WEBINAR` datetime DEFAULT NULL,
+  `MAKS_KAPASITAS` int(3) DEFAULT NULL,
+  `LINK_MEETING` text DEFAULT NULL,
+  `COVER_WEBINAR` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-alter table WEBINAR_KATEGORI add constraint FK_WEBINAR_KATEGORI2 foreign key (WEBINAR_ID)
-      references WEBINAR (WEBINAR_ID) on delete restrict on update restrict;
+-- --------------------------------------------------------
 
-alter table WEBINAR_REGIST add constraint FK_WEBINAR_REGIST foreign key (USER_ID)
-      references USER (USER_ID) on delete restrict on update restrict;
+--
+-- Struktur dari tabel `webinar_kategori`
+--
 
-alter table WEBINAR_REGIST add constraint FK_WEBINAR_REGIST2 foreign key (WEBINAR_ID)
-      references WEBINAR (WEBINAR_ID) on delete restrict on update restrict;
+CREATE TABLE `webinar_kategori` (
+  `KATEGORI_ID` int(3) NOT NULL,
+  `WEBINAR_ID` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `webinar_regist`
+--
+
+CREATE TABLE `webinar_regist` (
+  `USER_ID` int(255) NOT NULL,
+  `WEBINAR_ID` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `acc_webinar`
+--
+ALTER TABLE `acc_webinar`
+  ADD PRIMARY KEY (`USER_ID`,`WEBINAR_ID`),
+  ADD KEY `FK_ACC_WEBINAR2` (`WEBINAR_ID`);
+
+--
+-- Indeks untuk tabel `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`KATEGORI_ID`);
+
+--
+-- Indeks untuk tabel `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`USER_ID`);
+
+--
+-- Indeks untuk tabel `webinar`
+--
+ALTER TABLE `webinar`
+  ADD PRIMARY KEY (`WEBINAR_ID`),
+  ADD KEY `FK_CREATE_WEBINAR` (`USER_ID`);
+
+--
+-- Indeks untuk tabel `webinar_kategori`
+--
+ALTER TABLE `webinar_kategori`
+  ADD PRIMARY KEY (`KATEGORI_ID`,`WEBINAR_ID`),
+  ADD KEY `FK_WEBINAR_KATEGORI2` (`WEBINAR_ID`);
+
+--
+-- Indeks untuk tabel `webinar_regist`
+--
+ALTER TABLE `webinar_regist`
+  ADD PRIMARY KEY (`USER_ID`,`WEBINAR_ID`),
+  ADD KEY `FK_WEBINAR_REGIST2` (`WEBINAR_ID`);
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `acc_webinar`
+--
+ALTER TABLE `acc_webinar`
+  ADD CONSTRAINT `FK_ACC_WEBINAR` FOREIGN KEY (`USER_ID`) REFERENCES `user` (`USER_ID`),
+  ADD CONSTRAINT `FK_ACC_WEBINAR2` FOREIGN KEY (`WEBINAR_ID`) REFERENCES `webinar` (`WEBINAR_ID`);
+
+--
+-- Ketidakleluasaan untuk tabel `webinar`
+--
+ALTER TABLE `webinar`
+  ADD CONSTRAINT `FK_CREATE_WEBINAR` FOREIGN KEY (`USER_ID`) REFERENCES `user` (`USER_ID`);
+
+--
+-- Ketidakleluasaan untuk tabel `webinar_kategori`
+--
+ALTER TABLE `webinar_kategori`
+  ADD CONSTRAINT `FK_WEBINAR_KATEGORI` FOREIGN KEY (`KATEGORI_ID`) REFERENCES `kategori` (`KATEGORI_ID`),
+  ADD CONSTRAINT `FK_WEBINAR_KATEGORI2` FOREIGN KEY (`WEBINAR_ID`) REFERENCES `webinar` (`WEBINAR_ID`);
+
+--
+-- Ketidakleluasaan untuk tabel `webinar_regist`
+--
+ALTER TABLE `webinar_regist`
+  ADD CONSTRAINT `FK_WEBINAR_REGIST` FOREIGN KEY (`USER_ID`) REFERENCES `user` (`USER_ID`),
+  ADD CONSTRAINT `FK_WEBINAR_REGIST2` FOREIGN KEY (`WEBINAR_ID`) REFERENCES `webinar` (`WEBINAR_ID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
