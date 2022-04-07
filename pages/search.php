434f4cfeefa8233e393d8b9af43502e7d180507c
Akhmad Ramadani <ar20011118@gmail.com>
2022-04-07 14:22:56 +0700

    add search function and my event page dummies

--- pages/search.php
+++ pages/search.php
@@ -1,69 +1,31 @@
 <?php
 include './config/connection.php';
+$keyword = $_POST['keyword'];
 $query = "SELECT w.*, u.NAMA, u.EMAIL, COUNT(l.ID_LIKE) LIKE_COUNT 
 FROM webinar w JOIN user u ON w.USER_ID = u.USER_ID 
-LEFT JOIN like_webinar l ON w.WEBINAR_ID = l.WEBINAR_ID ORDER BY w.LOOKED DESC;";
+LEFT JOIN like_webinar l ON w.WEBINAR_ID = l.WEBINAR_ID
+HAVING (w.JUDUL_WEBINAR LIKE '%$keyword%' OR w.DESKRIPSI_WEBINAR LIKE '%$keyword%') 
+ORDER BY w.LOOKED DESC;";
+
 $fetcheddata = mysqli_query($connect, $query);
 
 ?>
 <div class="layout-px-spacing">
-    <div class="row mb-4 mt-3">
-        <div class="col-sm-3 col-12">
-            <div class="nav flex-column nav-pills mb-sm-0 mb-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-                <a class="nav-link active mb-2" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Coming Soon</a>
-                <a class="nav-link mb-2" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">On Progress</a>
-                <a class="nav-link mb-2" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">Finished</a>
-            </div>
-        </div>
 
-        <div class="col-sm-9 col-12">
-            <div class="tab-content" id="v-pills-tabContent">
-                <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
-                    <h4 class="mb-4">We move your world!</h4>
-                    <p class="mb-4">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    </p>
-
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    </p>
-                </div>
-                <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">
-                    <p class="dropcap  dc-outline-primary">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    </p>
-                </div>
-                <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">
-                    <blockquote class="blockquote">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                    </blockquote>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- <section class="section">
-        <div class="page-header px-4">
-            <div class="page-title">
-                <h3>Coming Soon</h3>
-            </div>
-
-            <div class="bg-primary rounded-circle p-1">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-right">
-                    <line x1="5" y1="12" x2="19" y2="12"></line>
-                    <polyline points="12 5 19 12 12 19"></polyline>
-                </svg>
-            </div>
-
-        </div>
-
+    <section class="section">
         <div class="container-fluid">
+            <?php
+            $jumlahdata = mysqli_num_rows($fetcheddata);
+            if ($jumlahdata <= 0) {
+            ?>
+
+                <div class="alert alert-primary mb-4" role="alert">
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><svg> ... </svg></button>
+                    <strong>Maaf!</strong> Webinar pada kategori tersebut belum ada
+                </div>
+            <?php
+            }
+            ?>
             <div class="port portfolio-masonry mt-4">
 
                 <div class="portfolioContainer row photo">
@@ -118,7 +80,7 @@ $fetcheddata = mysqli_query($connect, $query);
 
             </div>
         </div>
-    </section> -->
+    </section>
 
     <div class="footer-wrapper">
 
