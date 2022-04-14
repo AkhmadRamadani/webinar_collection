e72b0f33d0d2cfd8d3381b37990f02bfbf3ee829
AnnisaFitry <2041720123@student.polinema.ac.id>
2022-04-14 10:58:04 +0700

    updateInterface

--- webinar.php
+++ webinar.php
@@ -16,6 +16,14 @@
     <link href="styles/assets/css/scrollspyNav.css" rel="stylesheet" type="text/css" />
     <link href="styles/assets/css/components/cards/card.css" rel="stylesheet" type="text/css" />
     <link href="styles/assets/css/components/custom-media_object.css" rel="stylesheet" type="text/css" />
+    <!-- BEGIN THEME GLOBAL STYLES -->
+    <link href="styles/assets/css/scrollspyNav.css" rel="stylesheet" type="text/css" />
+    <link href="styles/plugins/animate/animate.css" rel="stylesheet" type="text/css" />
+    <script src="styles/plugins/sweetalerts/promise-polyfill.js"></script>
+    <link href="styles/plugins/sweetalerts/sweetalert2.min.css" rel="stylesheet" type="text/css" />
+    <link href="styles/plugins/sweetalerts/sweetalert.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/components/custom-sweetalert.css" rel="stylesheet" type="text/css" />
+    <!-- END THEME GLOBAL STYLES -->
 
     <?php
         $page = $_GET['page'];
@@ -216,16 +224,27 @@
                     <div class="widget-content widget-content-area">
                         <div class="row align-items-center">
                             <div class="col-lg-6">
-                                <div class="image-wrapper">
+                                <div class="image-wrapper mb-2">
                                     <img src="styles/assets/img/120x120.jpg" style="width: 550px; height: 550px;" alt>
+                                </div>
+                                <div>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-heart">
+                                        <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z">
+                                        </path>
+                                    </svg>&nbsp;--
+                                    &nbsp;&nbsp;&nbsp;
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-eye">
+                                        <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z">
+                                        </path>
+                                        <circle cx="12" cy="12" r="3"></circle>
+                                    </svg>&nbsp;--
                                 </div>
                             </div>
                             <div class="col-lg-6">
-                                <h6 class="">Event Date</h6>
-                                <h3 class="">Judul Webinar</h3>
-                                <p class="">Deskripsi webinar...</p>
-                                <br><br><br><br><br><br><br><br><br>
-                                <div class="image-wrapper avatar mt-2 mb-4">
+                                <div class="mt-1 mb-1" style="width: 550px; height: 20px;"><h6 class="">Event Date</h6></div>
+                                <div class="mt-1 mb-1" style="width: 550px; height: 50px;"><h3 class="">Judul Webinar</h3></div>
+                                <div class="mt-1 mb-1" style="width: 550px; height: 280px;"><p class="">Deskripsi webinar...</p></div>
+                                <div class="image-wrapper avatar mt-2 mb-4" style="width: 550px; height: 50px;">
                                     <img alt="avatar" src="styles/assets/img/90x90.jpg" class="rounded-circle" style="width: 50px; height: 50px;"/>&nbsp;&nbsp;&nbsp;Mentor Name
                                 </div>
                                 <button class="btn btn-primary btn-block mb-4 mr-2">Daftar</button>
@@ -256,7 +275,11 @@
     <!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM SCRIPTS -->
     <script src="styles/plugins/apex/apexcharts.min.js"></script>
     <script src="styles/assets/js/dashboard/dash_1.js"></script>
-
+    <!-- BEGIN THEME GLOBAL STYLE -->
+    <script src="styles/assets/js/scrollspyNav.js"></script>
+    <script src="styles/plugins/sweetalerts/sweetalert2.min.js"></script>
+    <script src="styles/plugins/sweetalerts/custom-sweetalert.js"></script>
+    <!-- END THEME GLOBAL STYLE -->
 </body>
 
 </html>
