6f95281dd4d1412c315e58d8856693aa420f660c
AnnisaFitry <2041720123@student.polinema.ac.id>
2022-06-01 16:44:29 +0700

    backed_error-mentorUpdateProfile

--- pages/mentor/mentor-profile.php
+++ pages/mentor/mentor-profile.php
@@ -21,15 +21,6 @@
         <!-- Card -->
         <div class="card component-card_4 user-profile">
             <div class="widget-content widget-content-area">
-                <div class="d-flex justify-content-between">
-                    <h3 class="">Info</h3>
-                    <a href="#" class="mt-2 edit-profile">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit-3">
-                            <path d="M12 20h9"></path>
-                            <path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"></path>
-                        </svg>
-                    </a>
-                </div>
                 <?php
                     if (isset($_SESSION["user"])) {
                         $akun = $_SESSION["user"];
@@ -40,6 +31,67 @@
 
                     while ($row = mysqli_fetch_assoc($result)) {
                 ?>
+                <div class="d-flex justify-content-between">
+                    <h3 class="">Info</h3>
+                    <a href="" data-toggle="modal" data-target="#updateModal<?php echo $row['USER_ID'] ?>" class="mt-2 edit-profile">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit-3">
+                            <path d="M12 20h9"></path>
+                            <path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"></path>
+                        </svg>
+                    </a>
+                </div>
+                
+                <!-- 
+                    UPDATE MODAL
+                -->
+                <div class="modal fade" id="updateModal<?php echo $row['USER_ID']; ?>" tabindex="-1" role="dialog" aria-labelledby="updateModalLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="updateModalLabel">Update Mentor</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <form class="form-vertical" enctype="multipart/form-data" action="" method="POST" id="formupdate<?php echo $row['USER_ID']; ?>">
+                                <div class="modal-body">
+
+                                    <input type="text" name="id_update_user" class="form-control" value="<?php echo $row['USER_ID'] ?>" hidden />
+
+                                    <div class="form-group mb-4">
+                                        <label class="control-label">Name:</label>
+                                        <input type="text" name="nama" class="form-control" value="<?php echo $row['NAMA'] ?>" />
+                                    </div>
+
+                                    <div class="form-group mb-4">
+                                        <label class="control-label">Email:</label>
+                                        <input type="text" name="email" class="form-control" value="<?php echo $row['EMAIL'] ?>" />
+                                    </div>
+
+                                    <div class="form-group mb-4">
+                                        <label class="control-label">Occupation:</label>
+                                        <input type="text" name="pekerjaan" class="form-control" value="<?php echo $row['PEKERJAAN'] ?>" />
+                                    </div>
+                                    <div class="form-group mb-4">
+                                        <label class="control-label">Bio:</label>
+                                        <input type="text" name="bio" class="form-control" value="<?php echo $row['BIODATA'] ?>" />
+                                    </div>
+                                    <div class="form-group mb-4">
+                                        <label class="control-label">Education:</label>
+                                        <input type="text" name="pendidikan" class="form-control" value="<?php echo $row['PENDIDIKAN'] ?>" />
+                                    </div>
+
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                    <input type="submit" class="btn btn-primary" value="Update" name="update" form="formupdate<?php echo $row['USER_ID']; ?>">
+
+                                </div>
+
+                            </form>
+                        </div>
+                    </div>
+                </div>
                 <div class="user-info">
                     <div class="text-center">
                         <img src="<?php echo $row['FOTO_PROFILE'];?>" alt="avatar" style="width:130px; height:130px; border-radius: 100%; object-fit: cover;">
@@ -82,6 +134,31 @@
     </div>
 </div>
 </div>
+<?php
+$id_user = $_POST['id_update_user'];
+$nama = $_POST['nama'];
+$email = $_POST['email'];
+$pekerjaan = $_POST['pekerjaan'];
+$bio = $_POST['bio'];
+$pendidikan = $_POST['pendidikan'];
+
+$update = $_POST['update'];
+$query_update = "UPDATE user, user_detail 
+                    SET user.NAMA='$nama', user.EMAIL='$email',
+                    user_detail.BIODATA ='$bio', user_detail.PENDIDIKAN ='$pendidikan', user_detail.PEKERJAAN='$pekerjaan' 
+                    WHERE user.USER_ID=user_detail.USER_ID AND user_detail.USER_ID ='$id_user';";
+
+if ($update) {
+    $up = mysqli_query($connect, $query_update);
+    if ($up) {
+        echo "<script>location='index.php?page=profile';</script>";
+    } else {
+        echo "<script>alert('Gagal menambahkan data'); </script>";
+        echo "<script>location='index.php?page=profile';</script>";
+    }
+}
+
+?>
 <!-- </div>
 </div>
 </div> -->
